80246bf487ace3e62e8d39ba271af108f7689951
Robin de Zeeuw <rdzeeuw@gmail.com>
2021-05-12 16:25:01 +0200

    Promises opdracht

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
-const log = console.log;
 
-const huiswerkMaken = (vak, callback) => {
-    setTimeout( function(){
-        log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
-      }, 500 );
-    
-    callback();
-}
+// exercise 1 ----- one promise------
+const testNum = num => {
+    return new Promise((resolve, reject) => {
+        if(num > 10){
+            resolve( num + ' is greater than 10');
+        } else {
+            reject( num + ' is less than 10');
+        }  
+    });
+};
 
-const klaarMetHuiswerk = () => {
-    log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!")
-}
+testNum(9)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
 
-huiswerkMaken("wiskunde", klaarMetHuiswerk);
+testNum(15)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+// exercise 2 -----promise.all()----
+const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
+const complicatedArray = ['cucumber', 44, true];
+
+const makeAllCaps = words => {
+    return new Promise((resolve, reject) => {
+        if(words.every(word => {
+            return typeof word === 'string';
+        })
+        ){
+            resolve(
+                sortWords(
+                    words.map(word => {
+                        return word.toUpperCase();
+                    })
+                )
+            );
+        } else {
+            reject('Not a string!');
+        }
+    });
+};
+
+const sortWords = words => {
+    return new Promise((resolve, reject) => {
+        if(words) {
+            resolve(words.sort());
+        } else {
+            reject('strings only!');
+        }
+    });
+};
+
+makeAllCaps(arrayOfWords)
+    .then(sortWords(arrayOfWords))
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+makeAllCaps(complicatedArray)
+    .then(sortWords(complicatedArray))
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
